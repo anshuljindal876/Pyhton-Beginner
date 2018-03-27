e620fa12bfdc52694bbdc65e33fe2ea292845a2b
Anshul Jindal <anshuljindal876@gmail.com>
2018-03-27 13:42:28 +0530

    Added name stamp Expanded for-loop test code

--- for_loop.py
+++ for_loop.py
@@ -1,3 +1,14 @@
+#Code that explains basic working of for loop
+#By- Anshul Jindal
+
+num = [1,2,3,4,5]
 word = ['cat', 'dog', 'horse']
+num_word = [1,2,3,'cat','dog']
+for w in num:
+    print(w, end = ',')
+print()
 for w in word:
-    print(w, len(w))
+    print(w, len(w), end = '..')
+print()
+for w in num_word:
+    print(w, end = '-')
